3399bb8e0f72d843b62f01451a82508c75f6cb4a
Kishan Jaiswal <kj8092943@gmail.com>
2024-06-30 17:39:54 +0530

    updated

--- Backend/middleware/userAuth.js
+++ Backend/middleware/userAuth.js
@@ -7,7 +7,7 @@ export const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  console.log(token);
+  console.log("Token", token);
   if (token == null) {
     return res.status(401).json({ message: "Authentication token required" });
   }
